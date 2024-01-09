27d639b50393f2e60bdf3c36e4e37b9f6af90bcb
Dhanarooban1 <dhanazx1@gmail.com>
2024-01-09 14:58:47 +0530

    done

---
+++ Counter App/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const add = ()=>{
+      setCount((prev)=>prev+1)
+     
+  }
+
+  const sub = () =>{
+    setCount((count)=> count <=0 ? 0: count-1)
+  }
+
+const reset = () =>{
+  setCount((count)=>setCount(0)) 
+}
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <div className = "Display_src">{count}</div>
+      <div className = "btn">
+      <div className = "add_btn"><button onClick={add}>+</button></div>
+      <div className = "sub_btn"><button onClick={sub}>-</button></div>
+      <div className = "reset_btn"><button onClick={reset}>RESET</button></div>
+      </div>
+    </>
+  )
+}
+ 
+export default App
+
+
